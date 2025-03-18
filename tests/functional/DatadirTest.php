dd8b46a6f4ec54001143d13c1b7d9c97ba0bd3eb
Marek Šuca <mark.suca@gmail.com>
2025-03-18 11:24:19 +0100

    dataDir tests
    test case
    
    tables & state
    
    out state as well

---
+++ tests/functional/DatadirTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\SnowflakeQueryHistory\FunctionalTests;
+
+use Keboola\DatadirTests\DatadirTestCase;
+
+class DatadirTest extends DatadirTestCase
+{
+}
